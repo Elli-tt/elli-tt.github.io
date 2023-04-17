92335163ac48967ae2587edddf9127e4f3518e33
Elli-tt <elliottjoiner@me.com>
2023-04-17 20:54:41 +0100

    initial upload

--- js/elplugin.js
+++
@@ -1,9 +0,0 @@
-function clickedFreeButton() {
-    var freeList = document.getElementById("tm-gallery-page-free");
-    var supporterList = document.getElementById("tm-gallery-page-supporter");
-    var otherList = document.getElementById("tm-gallery-page-other");
-    
-    freeList.addClass('hidden');
-    supporterList.removeClass('hidden');
-    otherList.removeClass('hidden');
-  }
